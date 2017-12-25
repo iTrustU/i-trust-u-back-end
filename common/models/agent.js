3d632e521ef6047d681b2d25a1f87b2be5b7aac6
Diky Arga <dikyarga.id@gmail.com>
2017-12-25 11:43:40 +0700

    add an object into algolia when data saved

--- common/models/agent.js
+++ common/models/agent.js
@@ -3,15 +3,17 @@
 const algoliasearch = require('algoliasearch');
 
 let client = algoliasearch('4HJ26M6YLI', '227758b75aa72cb9749c2db4004f5dce');
+let index = client.initIndex('iTrustU-demo');
 
 module.exports = function(Agent) {
   Agent.observe('after save', function logQuery(ctx, next) {
     if (ctx.instance) {
       console.log('ctx.instance : ', ctx.instance);
 
-
-      
-
+      index.addObject(ctx.instance, (err, content) => {
+        console.log('the content was submitted : ', content);
+      });
     }
-  })
+    next();
+  });
 };
