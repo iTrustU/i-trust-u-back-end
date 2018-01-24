afe76420eecc79730a6eb089d5fe38a356dbb20a
Diky Arga <dikyarga.id@gmail.com>
2018-01-25 02:11:32 +0700

    move airtable setup to make more modular

---
+++ server/airtable-setup.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var Airtable = require('airtable');
+
+var base = new Airtable({
+  apiKey: process.env.AIRTABLE_API_KEY,
+}).base(process.env.AIRTABLE_BASE);
+
+module.exports = base;
